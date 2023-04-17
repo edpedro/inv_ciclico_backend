b6fd3f230588e33c8da6c8ac3bee262a01bcb0bb
Eduardo <edp2013.ep@gmail.com>
2023-04-17 20:45:30 -0300

    update

--- prisma/migrations/20230329002809_add_innullable/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "baseInventario" DROP CONSTRAINT "baseInventario_username_id_fkey";
-
--- AlterTable
-ALTER TABLE "baseInventario" ALTER COLUMN "username_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "baseInventario" ADD CONSTRAINT "baseInventario_username_id_fkey" FOREIGN KEY ("username_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
