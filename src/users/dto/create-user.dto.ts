3803477e167c6d327020cb0989ec2dc643e49aae
Eduardo <edp2013.ep@gmail.com>
2023-09-13 21:06:29 -0300

    implementacao class-validator users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,23 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: "O campo 'Nome' deve ser uma string." })
+  @IsNotEmpty({ message: "O campo 'Nome' não pode estar vazio." })
   readonly name: string;
 
+  @IsString({ message: "O campo 'Usuario' deve ser uma string." })
+  @IsNotEmpty({ message: "O campo 'Usuario' não pode estar vazio." })
   readonly username: string;
 
+  @IsString({ message: "O campo 'Senha' deve ser uma string." })
+  @IsNotEmpty({ message: "O campo 'Senha' não pode estar vazio." })
   readonly password: string;
 
+  @IsString({ message: "O campo 'role' deve ser uma string." })
+  @IsNotEmpty({ message: "O campo 'role' não pode estar vazio." })
   readonly role: string;
 
+  @IsString({ message: "O campo 'createdById' deve ser uma string." })
+  @IsOptional()
   readonly createdById?: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  readonly name: string;
-
-  readonly username: string;
-
-  readonly password?: string;
-
-  readonly role: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
