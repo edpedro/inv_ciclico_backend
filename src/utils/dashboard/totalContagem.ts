89be9c9ab95fe7979c4f753466872895b066eea4
Eduardo <edp2013.ep@gmail.com>
2024-03-12 20:17:30 -0300

    Ajuste total de contagem

--- src/utils/dashboard/totalContagem.ts
+++ src/utils/dashboard/totalContagem.ts
@@ -1,11 +1,14 @@
 import { ListDashboardDto } from '../../dashboard/dto/list-dashboard.dto';
+import { RemoveDuplicatesEndereco } from './removeDuplicatesEndereco';
 export async function TotalContagem(data: ListDashboardDto[]) {
   // total de 2 segunda contagem
   let totalSegundaContagem = 0;
   //Total de 1 contagem
   let totalPrimeiraContagem = 0;
 
-  data.forEach((value) => {
+  const removeDuplicatesEndereco = await RemoveDuplicatesEndereco(data);
+
+  removeDuplicatesEndereco.forEach((value) => {
     if (value.secondCount !== null) {
       totalSegundaContagem = totalSegundaContagem + 1;
     }
